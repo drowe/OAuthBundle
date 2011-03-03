b1b4cf60f83e25bcba1848f4e9040ad2c8793e09
Dave Rowe <dave@roweware.com>
2011-03-02 22:50:44 -0600

    Bundle class requires 2 methods

--- OAuthBundle.php
+++ OAuthBundle.php
@@ -6,4 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OAuthBundle extends Bundle
 {
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+    
+    public function getPath()
+    {
+        return strtr(__DIR__, "\\", "/");
+    }
 }
